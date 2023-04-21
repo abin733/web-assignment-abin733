5b4182a0cb947e2100ebd1759da8f6e9ff1b9203
abin733 <abin733@aucklanduni.ac.nz>
2023-04-21 16:56:58 +0800

    20230421_1656

--- app.js
+++ app.js
@@ -23,15 +23,39 @@ const carData = require("./car-data");
 // When a GET request is made to "/" (i.e. the root path), render the
 // "home" view. This can be found in /views/home.handlebars
 app.get("/", function (req, res) {
-    res.render("home");
+    const uniqueCountries = getUniqueCountries().sort();
+    const uniqueIds = getUniqueIds();
+    res.render("home", { cars: carData, countries: uniqueCountries , carId:uniqueIds});
 
 });
 
+app.get("/cars/years", function(req,res){
+    console.log("Request received to 'cars/years' ");
+    console.log(JSON.stringify(req.query));
+
+    const lowerYear = req.query.lower;
+    const upperYear = req.query.upper;
+
+    console.log(`lower: ${lowerYear} upper: ${upperYear}`);
+    //TODO: create function to get cars by range of years
+    const carsArray = getCarsByYears(lowerYear,upperYear);
+    res.locals.cars = carsArray;
+
+    //TODO: res.render & create Handlebars file
+    res.render("carsbyyear");
+});
+
 // An example route handler function that demonstrates how car data can be sent to the web browser
-app.get("/cars", function (req, res) {
+app.get("/cars/country", function (req, res) {
     const countryName = req.query.country;
     const cars = getCarsByCountry(countryName);
     res.json(cars);
+});
+
+app.get("/cars/id", function (req, res) {
+    const carsId = req.query.carId;
+    const selectedId = getCarsById(carsId);
+    res.json(selectedId);
 });
 
 // Start the server running. Once the server is running, the given function will be called, which will
@@ -43,8 +67,60 @@ app.listen(port, function () {
 
 // An example function that gets all cars from a given country
 function getCarsByCountry(countryName){
+    if (!countryName) {
+        return carData;
+    }
     const carsFromCountry = carData.filter((car) => car.country === countryName);
     return carsFromCountry;
 }
+
+function getCarsById(carId){
+    console.log("get cars by id:" + carId)
+    let carsId = [];
+    if (!carId) {
+        return carData;
+    }
+    for (let i = 0; i < carData.length; i++) {
+        if(carData[i].id == carId){
+            console.log("Matched");
+            carsId.push(carData[i]);
+        }
+
+    }
+    console.log(JSON.stringify(carsId))
+    return carsId;
+}
+
+function getCarsByYears(lower,upper){
+    const filteredResults = carData.filter(function (car) {
+        return car.carYear >= lower && car.carYear <= upper;       
+    });
+    return filteredResults;
+}
+
+function getUniqueCountries() {
+    const countries = carData.map((car) => car.country);
+    return [...new Set(countries)];
+}
+
+function getUniqueIds() {
+    const carsId = carData.map((car) => car.id);
+    return [...new Set(carsId)];
+}
+
+// Function to get cars by lower and upper years...
+// function getCarsByYears(lower,upper){
+//     let filteredResults = [];
+//     for (let i = 0; i < carData.length; i++) {
+//         if(carData[i].carYear >= lower && carData[i].carYear <= upper){
+//             filteredResults.push(carData[i]);
+//         }
+
+//     }
+//     // Test filtered results
+//     //console.log(JSON.stringify(filteredResults));
+//     return filteredResults;
+// }
+
 
 
